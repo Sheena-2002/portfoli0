52fd3189e4d380485c3de51447f35f1b4db254ae
Sheena-2002 <sheena02robo@gmail.com>
2026-06-23 18:04:00 -0500

    june_update

--- webpage_11/script.js
+++ webpage_11/script.js
@@ -1,13 +1,13 @@
 const labFiles = {
-    1: { post: 'pdfs/Post_Lab_1_Sheena_S.pdf' },
-    2: { post: 'pdfs/Post_Lab_2_Sheena_S.pdf' },
-    3: { pre: 'pdfs/Pre_Lab_3_Sheena_S.pdf', post: 'pdfs/Post_Lab_3_Sheena_S.pdf' },
-    4: { post: 'pdfs/Post_lab_4_Sheena_S.pdf' },
-    5: { pre: 'pdfs/Pre_lab_5_Sheena_S.pdf', post: 'pdfs/post_lab_5_Sheena_S.pdf' },
-    6: { pre: 'pdfs/Pre_lab_6_Sheena_S.pdf', post: 'pdfs/Post_Lab_6_Sheena_S.pdf' },
-    7: { pre: 'pdfs/Pre_lab_7_Sheena_S.pdf', post: 'pdfs/Post_Lab_7_Sheena_S.pdf' },
-    8: { pre: 'pdfs/Pre_lab_8_Sheena_S.pdf', post: 'pdfs/Post_Lab_8_Sheena_S.pdf' },
-    9: { pre: 'pdfs/Pre_lab_9_Sheena_S.pdf', post: 'pdfs/Post_Lab_9_Sheena_S.pdf' }
+    1: { post: 'pdfs/Post_Lab_1_Sheena_S.pdf', manual: 'pdfs/Lab1.pdf' },
+    2: { post: 'pdfs/Post_Lab_2_Sheena_S.pdf', manual: 'pdfs/Lab2.pdf' },
+    3: { pre: 'pdfs/Pre_Lab_3_Sheena_S.pdf', post: 'pdfs/Post_Lab_3_Sheena_S.pdf', manual: 'pdfs/Lab3.pdf' },
+    4: { post: 'pdfs/Post_lab_4_Sheena_S.pdf', manual: 'pdfs/Lab4.pdf' },
+    5: { pre: 'pdfs/Pre_lab_5_Sheena_S.pdf', post: 'pdfs/post_lab_5_Sheena_S.pdf', manual: 'pdfs/Lab5.pdf' },
+    6: { pre: 'pdfs/Pre_lab_6_Sheena_S.pdf', post: 'pdfs/Post_Lab_6_Sheena_S.pdf', manual: 'pdfs/Lab6.pdf' },
+    7: { pre: 'pdfs/Pre_lab_7_Sheena_S.pdf', post: 'pdfs/Post_Lab_7_Sheena_S.pdf', manual: 'pdfs/Lab7.pdf' },
+    8: { pre: 'pdfs/Pre_lab_8_Sheena_S.pdf', post: 'pdfs/Post_Lab_8_Sheena_S.pdf', manual: 'pdfs/Lab8.pdf' },
+    9: { pre: 'pdfs/Pre_lab_9_Sheena_S.pdf', post: 'pdfs/Post_Lab_9_Sheena_S.pdf', manual: 'pdfs/Lab9.pdf' }
 };
 const projectPdf = 'pdfs/PROJECT_704_REPORT.pdf';
 
